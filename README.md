07fcd8b13007999349a8474c7fd6cd17216ee9c5
Marta Czernik <czernik.m@wp.pl>
2016-04-08 14:13:25 +0200

    RWD + form

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sit-on-Chair-2
+
+My first website:
+
+-RWD
+
+-Partly based on flexbox
+
+-Active areas:
+
+    *Slider (JS);
+    
+    *Disappearing avigation (JS);
+    
+    *Hamburger menu (Jquery);
+    
+    *Form (Jquery);
--- js/app_jquery.js
+++ js/app_jquery.js
@@ -11,22 +11,28 @@ $(function () {
     //Rodzaj krzesła
   $("#chair_type").on("change", function () {
     var text = $("#chair_type option:selected").text();
-    var priceChair = $("#chair_type").prop("value") + "ZØ";
-    var price = parseFloat(priceChair)
-    $("#chair_choice").html(text)
-    $("#price").html(price)
+    var priceChair = $("#chair_type").prop("value");
+    var price = parseFloat(priceChair);
+    $("#chair_choice").html(text);
+    $("#price").html(price);
   })
 
   //Kolor
   $("#chair_color").on("change", function () {
     var text = $("#chair_color option:selected").text();
+    var priceColor = $("#chair_color").prop("value");
+    var price = parseFloat(priceColor);
     $("#chairColorTxt").html(text)
+    $("#price1").html(price);
   })
 
   //Materiał
   $("#fabric").on("change", function () {
     var text = $("#fabric option:selected").text();
-    $("#choice_fabrics").html(text)
+    var priceModel = $("#fabric").prop("value");
+    var price = parseFloat(priceModel);
+    $("#choice_fabrics").html(text);
+    $("#price2").html(price);
   })
 
   //Transport
@@ -43,8 +49,14 @@ $(function () {
   $(".form").on("change", function () {
     var suma1 = parseFloat($("#price").html());
     var suma2 = parseFloat($("#transport_price").html());
+    var suma3 = parseFloat($("#price1").html());
+    var suma4 = parseFloat($("#price2").html());
     var suma = suma1 + suma2;
     console.log(typeof suma)
-    $("#amount").html(suma1 + suma2)
+    if($(".trans input").is(":checked")) {
+    $("#amount").html(suma1 + suma2 + suma3 + suma4)
+    } else{
+      $("#amount").html(suma1 + suma3 + suma4)
+    }
   })
 })
